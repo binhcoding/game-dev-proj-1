ecbd9acd05d19a6dc0f2f51f631dd7c1076dafa0
binhcoding <ihave@binhcoding.com>
2015-02-17 21:36:07 -0800

    committing sln/vcproj

---
+++ Game Dev Project 1 - 2D/Game.cpp
@@ -0,0 +1,27 @@
+#include "Game.h"
+#include <iostream>
+#include <string>
+#include <Game Engine/Errors.h>
+#include <Game Engine/GameEngine.h>
+#include <Game Engine/ResourceManager.h>
+#include <Game Engine/Widget.h>
+
+Game::Game()
+{
+}
+
+
+Game::~Game()
+{
+}
+
+void Game::Run()
+{
+	Init();
+}
+
+void Game::Init()
+{
+	GameEngine::Init();
+	_window.Create("Game Engine", _screenWidth, _screenHeight, 0);
+}
---
+++ Game Dev Project 1 - 2D/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+#include <Game Engine/Sprite.h>
+#include <Game Engine/GLSLProgram.h>
+#include <Game Engine/GLTexture.h>
+#include <Game Engine/Window.h>
+#include <vector>
+#include <Game Engine/Camera2D.h>
+#include <Game Engine/SpriteBatch.h>
+#include <Game Engine/InputManager.h>
+#include <Game Engine/Timing.h>
+#include <Game Engine/Widget.h>
+
+class Game
+{
+	public:
+	Game();
+	~Game();
+	void Run();
+	private:
+	void Init();
+
+	GameEngine::Window _window;
+	int _screenWidth;
+	int _screenHeight;
+};
+
